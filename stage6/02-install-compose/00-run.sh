b549e4e65cc9ecdc3dbc5fed0153f913c2abe56a
Matti Airas <matti.airas@hatlabs.fi>
2024-04-01 19:46:09 +0300

    Install signalk server docker compose file

---
+++ stage6/02-install-compose/00-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# install signalk-server docker-compose.yml under /opt/containers/signalk-server
+mkdir -p "${ROOTFS_DIR}/opt/containers/signalk-server"
+install -m 644 files/signalk-server-docker-compose.yml "${ROOTFS_DIR}/opt/containers/signalk-server/docker-compose.yml"
+
+# Create a data directory for signalk-server with FIRST_USER_NAME as owner
+on_chroot << EOF
+mkdir -p /opt/containers/signalk-server/data/{grafana,influxdb2,signalk}
+chown -R $FIRST_USER_NAME:$FIRST_USER_NAME /opt/containers/signalk-server/data/{influxdb2,signalk}
+# FIXME: find a better way to handle this
+chown -R 472 /opt/containers/signalk-server/data/grafana
+EOF
+
+# Enable docker-compose@signalk-server service
+on_chroot << EOF
+systemctl enable docker-compose@signalk-server
+EOF
